8e662ca209e43d900cda53ffef93197ab96d6892
cbattenplowright <callumbp15@gmail.com>
2023-08-09 22:26:05 +0100

    initial commit

---
+++ src/test/java/com/bnta/airlineAPI/AirlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.airlineAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
