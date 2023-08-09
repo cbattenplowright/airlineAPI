4d00ff2fd0be92013eb73659163f6e85177801f6
cbattenplowright <callumbp15@gmail.com>
2023-08-09 23:18:22 +0100

    created FlightService with get methods

---
+++ src/main/java/com/bnta/airlineAPI/services/FlightService.java
@@ -0,0 +1,23 @@
+package com.bnta.airlineAPI.services;
+
+import com.bnta.airlineAPI.models.Flight;
+import com.bnta.airlineAPI.repositories.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights(){
+        return flightRepository.findAll();
+    }
+
+    public Flight getFlightById(Long id) {
+        return flightRepository.findById(id).get();
+    }
+}
