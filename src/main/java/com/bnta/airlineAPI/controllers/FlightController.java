7b674c4125a1d118eaaeca3449f02e3c24698f43
cbattenplowright <callumbp15@gmail.com>
2023-08-09 23:46:35 +0100

    created FlightController with GET Mappings

---
+++ src/main/java/com/bnta/airlineAPI/controllers/FlightController.java
@@ -0,0 +1,31 @@
+package com.bnta.airlineAPI.controllers;
+
+import com.bnta.airlineAPI.models.Flight;
+import com.bnta.airlineAPI.services.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("flights")
+public class FlightController {
+
+    @Autowired
+    FlightService flightService;
+
+//    GET localhost:8080/flights
+//    INDEX
+    @GetMapping
+    public ResponseEntity<List<Flight>> getAllFlights(){
+        return new ResponseEntity<>(flightService.getAllFlights(), HttpStatus.OK);
+    }
+    
+//    GET localhost:8080/flights/1
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Flight> getFlightById(@PathVariable Long id){
+        return new ResponseEntity<>(flightService.getFlightById(id), HttpStatus.OK);
+    }
+}
