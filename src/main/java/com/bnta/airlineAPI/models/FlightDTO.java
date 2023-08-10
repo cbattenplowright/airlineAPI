aa46da3208fd19ec6d199d63a4af7e62372a56c4
cbattenplowright <callumbp15@gmail.com>
2023-08-10 10:31:43 +0100

    added FlightDTO

---
+++ src/main/java/com/bnta/airlineAPI/models/FlightDTO.java
@@ -0,0 +1,50 @@
+package com.bnta.airlineAPI.models;
+
+
+
+public class FlightDTO {
+
+    private String destination;
+    private int capacity;
+    private String departureDate;
+    private String departureTime;
+
+    public FlightDTO(String destination, int capacity, String departureDate, String departureTime){
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+}
